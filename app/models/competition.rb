c68280382b50e93734132ebf64c21afc4dd68fc6
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-02 04:46:25 -0300

    feat: implemented validations on competition model

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,9 +1,14 @@
 class Competition < ApplicationRecord
+    belongs_to :user
+
+    validates: :name, :end_date, :unit_of_measurement, presence: true
+    validates: :name, uniqueness: true
+    validates: :unit_of_measurement, inclusion: { in: %w(seconds meters points) }
+    validates: :end_date, date: { after_or_equal_to: Proc.new { Date.today }, message: 'must be after or equal to today' }
+
     enum unit_of_measurement: {
         seconds: 'seconds',
         meters: 'meters',
         points: 'points'
     }
-
-    validates :name, :end_date, :unit_of_measurement, presence :true
 end
