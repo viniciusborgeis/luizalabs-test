0310fa3c758f8cb7582b73d257f588ece7a0fd8c
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-02 05:24:13 -0300

    refactor: adjusted validates, removed ":", created participant association

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,10 +1,11 @@
 class Competition < ApplicationRecord
     belongs_to :user
+    has_many :participant
 
-    validates: :name, :end_date, :unit_of_measurement, presence: true
-    validates: :name, uniqueness: true
-    validates: :unit_of_measurement, inclusion: { in: %w(seconds meters points) }
-    validates: :end_date, date: { after_or_equal_to: Proc.new { Date.today }, message: 'must be after or equal to today' }
+    validates :name, :end_date, :unit_of_measurement, presence: true
+    validates :name, uniqueness: true
+    validates :unit_of_measurement, inclusion: { in: %w(seconds meters points) }
+    # validates :end_date, date: { after_or_equal_to: Proc.new { Date.today }, message: 'must be after or equal to today' }
 
     enum unit_of_measurement: {
         seconds: 'seconds',
