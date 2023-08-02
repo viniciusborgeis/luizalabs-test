20ee0c7a23c0dca2c614e78b107b4e84281e19b1
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-02 05:26:06 -0300

    feat: created association with participants and removed "depend: destroy" from relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  has_many :competitions, dependent: :destroy
+  has_many :competitions
+  has_many :participants
+  has_many :participated_competitions, through: :participants, source: :competition
 
   validates :name, :role, presence: true
   validates :password, length: 6..15
