10bc5b4d7a8fb88d0694d95c0d909da3683ea721
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-03 01:30:51 -0300

    refactor: changed register and sessions use cases for a new folder called users inside use_case folder

---
+++ app/use_cases/users/logout_usecase.rb
@@ -0,0 +1,20 @@
+class Users::LogoutUsecase
+    def initialize(token)
+        @token = token
+    end
+
+    def execute
+        user = User.find_by(jti: decoded_token['jti'])
+        return unless user
+        
+        user
+    end
+
+    private
+
+    attr_reader :token
+
+    def decoded_token
+       JWT.decode(token.split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first     
+    end
+end
