0dfd04159bd1e8bb82c48989a45a303cb7d1f30b
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-03 01:34:40 -0300

    feat: created a validate for check if end date is in the past

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -6,11 +6,20 @@ class Competition < ApplicationRecord
     validates :name, :end_date, :unit_of_measurement, presence: true
     validates :name, uniqueness: true
     validates :unit_of_measurement, inclusion: { in: %w(seconds meters points) }
-    # validates :end_date, date: { after_or_equal_to: Proc.new { Date.today }, message: 'must be after or equal to today' }
+    validate :end_date_cannot_be_in_the_past
 
     enum unit_of_measurement: {
         seconds: 'seconds',
         meters: 'meters',
         points: 'points'
     }
+
+    private 
+
+    def end_date_cannot_be_in_the_past
+        if end_date.present? && end_date < Date.today
+            errors.add(:end_date, "must be after or equal to today")
+        end
+    end   
 end
+
