c625e71763c29384f142b70d741b4216acee8d1e
Vinicius Borges <viniciusborgeis@gmail.com>
2023-08-03 04:03:30 -0300

    refactor: removed unit into competition table and created a new field called closed

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_083404) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_050023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "competitions", force: :cascade do |t|
     t.string "name"
-    t.datetime "end_date"
-    t.string "unit_of_measurement"
+    t.boolean "closed", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "modality_id", null: false
+    t.index ["modality_id"], name: "index_competitions_on_modality_id"
     t.index ["user_id"], name: "index_competitions_on_user_id"
+  end
+
+  create_table "modalities", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "unit", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "participants", force: :cascade do |t|
@@ -59,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_083404) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "competitions", "modalities"
   add_foreign_key "competitions", "users"
   add_foreign_key "participants", "competitions"
   add_foreign_key "participants", "users"
